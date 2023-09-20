8f3d18d444d56c35ddb8e3d8bf76c8a79fa9c977
윤예지 <hallo27@naver.com>
2023-09-20 16:00:30 +0900

    feat: 저금통 생성 페이지 및 관련 Provider 추가 (#S09P22C207-216)

--- client/lib/screens/sample_code_page/make_piggy_sample_page.dart
+++ client/lib/screens/sample_code_page/make_piggy_sample_page.dart
@@ -97,23 +97,23 @@ class _MakePiggySamplePageState extends State<MakePiggySamplePage> {
 }
 
 Future<dynamic> patchPiggyImage(dynamic headers, dynamic baseUri, dynamic data) async {
-    print("프로필 사진 서버에 업로드");
-    var dio = Dio();
-    try {
-      dio.options.baseUrl = baseUri;
-      dio.options.contentType = 'multipart/form-data';
-      dio.options.maxRedirects.isFinite;
-      dio.options.connectTimeout = Duration(seconds: 5);
-      dio.options.receiveTimeout = Duration(seconds: 3);
+  print("프로필 사진 서버에 업로드");
+  var dio = Dio();
+  try {
+    dio.options.baseUrl = baseUri;
+    dio.options.contentType = 'multipart/form-data';
+    dio.options.maxRedirects.isFinite;
+    dio.options.connectTimeout = Duration(seconds: 5);
+    dio.options.receiveTimeout = Duration(seconds: 3);
 
-      dio.options.headers = headers;
-      var response = await dio.post(
-        '/bank-service/piggy/yoonyeji',
-        data: data
-      );
-      print('업로드 성공');
-      return response.data;
-    } catch (e) {
-      print('Error during HTTP request: $e');
-    }
+    dio.options.headers = headers;
+    var response = await dio.post(
+      '/bank-service/piggy/yoonyeji',
+      data: data
+    );
+    print('업로드 성공');
+    return response.data;
+  } catch (e) {
+    print('Error during HTTP request: $e');
   }
+}
